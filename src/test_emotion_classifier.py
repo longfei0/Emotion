706698fe04c693cc57d4db094ea36dda65fca0e2
Bruce Lee <420538167@qq.com>
2019-05-09 07:30:19 +0800

    Add files via upload

---
+++ src/test_emotion_classifier.py
@@ -0,0 +1,56 @@
+from models.cnn import mini_XCEPTION
+from utils.datasets import DataManager
+from utils.datasets import split_data
+
+
+batch_size = 32
+num_epochs = 10000
+input_shape = (64, 64, 1)
+validation_split = .2
+verbose = 1
+num_classes = 7
+patience = 50
+
+# data generator
+data_generator = ImageDataGenerator(
+                        featurewise_center=False,
+                        featurewise_std_normalization=False,
+                        rotation_range=10,
+                        width_shift_range=0.1,
+                        height_shift_range=0.1,
+                        zoom_range=.1,
+                        horizontal_flip=True)
+
+# model parameters/compilation
+model = mini_XCEPTION(input_shape, num_classes)
+model.compile(optimizer='adam', loss='categorical_crossentropy',
+              metrics=['accuracy'])
+model.summary()
+
+datasets = ['fer2013']
+for dataset_name in datasets:
+
+
+	 print('Training dataset:', dataset_name)
+
+    # callbacks
+    log_file_path = base_path + dataset_name + '_emotion_training.log'
+    csv_logger = CSVLogger(log_file_path, append=False)
+    early_stop = EarlyStopping('val_loss', patience=patience)
+    reduce_lr = ReduceLROnPlateau('val_loss', factor=0.1,
+                                  patience=int(patience/4), verbose=1)
+    trained_models_path = base_path + dataset_name + '_mini_XCEPTION'
+    model_names = trained_models_path + '.{epoch:02d}-{val_acc:.2f}.hdf5'
+    model_checkpoint = ModelCheckpoint(model_names, 'val_loss', verbose=1,
+                                                    save_best_only=True)
+    callbacks = [model_checkpoint, csv_logger, early_stop, reduce_lr]
+
+    # loading dataset
+    data_loader = DataManager(dataset_name, image_size=input_shape[:2])
+    faces, emotions = data_loader.get_data()
+    faces = preprocess_input(faces)
+    num_samples, num_classes = emotions.shape
+    train_data, val_data = split_data(faces, emotions, validation_split)
+    val_face, val_emotion = val_dataloss, accuracy = model.evaluate(val_face, val_emotion)
+	print('\ntest loss: ', loss)
+	print('\ntest accuracy: ', accuracy)
